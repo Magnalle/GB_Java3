3f8c64a3988beb8c41f3c27f842a6e6b9e385f65
Valentina <yasu@list.ru>
2020-11-29 15:07:13 +0700

    Task 5 start

---
+++ src/task_5_1/Race.java
@@ -0,0 +1,11 @@
+package task_5_1;
+import java.util.*;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
+
