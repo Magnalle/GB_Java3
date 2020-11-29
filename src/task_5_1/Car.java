4a56efbd3cbe1903c6cf6a97c0e414adb4006c06
Valentina <yasu@list.ru>
2020-11-29 15:36:59 +0700

    Task 5 completed

--- src/task_5_1/Car.java
+++ src/task_5_1/Car.java
@@ -1,12 +1,19 @@
 package task_5_1;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    //public static CountDownLatch cdl = null;
+    public static CyclicBarrier cbStart = null, cbEnd = null;
+    public static Semaphore sem = null;
     static {
         CARS_COUNT = 0;
     }
+    private static volatile boolean startSent = false;
+    private static Object startLock = new Object();
+    private static volatile boolean endSent = false;
+    private static Object endLock = new Object();;
 
     private Race race;
     private int speed;
@@ -29,11 +36,37 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cbStart.await();
+            synchronized (startLock){
+                if(!startSent){
+                    System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+                    startSent = true;
+                }
+            }
+//            cdl.countDown();
+//            try {
+//                cdl.await();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        try {
+            cbStart.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        synchronized (endLock) {
+            if (!endSent) {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+                endSent = true;
+            }
         }
     }
 }
